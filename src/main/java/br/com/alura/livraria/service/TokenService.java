dd54b109853ed530d898dd2c62d5b0850d61902e
anadeso <anadeso90@gmail.com>
2021-11-03 07:31:59 -0300

    Alterando autenticacao para stateless via token

---
+++ src/main/java/br/com/alura/livraria/service/TokenService.java
@@ -0,0 +1,24 @@
+package br.com.alura.livraria.service;
+
+import br.com.alura.livraria.entities.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+
+    @Value("${jjwt.secret}")
+    private String secret;
+
+    public String gerarToken(Authentication authentication) {
+        Usuario logado = (Usuario) authentication.getPrincipal();
+        return Jwts
+                .builder()
+                .setId(logado.getId().toString())
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+}
